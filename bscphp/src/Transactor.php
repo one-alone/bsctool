b755c8cf52cb17f149ec4bbf1dbf51745b61fba5
周末的黄昏 <2924953@qq.com>
2023-03-06 17:47:04 +0800

    增加demo文件  增加可自定义修改RPC链接方式

--- bscphp/src/Transactor.php
+++ bscphp/src/Transactor.php
@@ -6,111 +6,112 @@ use Web3\Contract;
 use Web3\Utils;
 
 class Transactor{
-  protected $web3;
-  
-  //context
-  protected $gasPrice;
-  protected $gasLimit;
-  protected $value;
-  protected $credential;
-    
-  public function __construct($web3, $credential){
-    $this->web3 = $web3;
-    $this->credential = $credential;    
-  }
-  
-  public function setGasPrice($price = null){
-    $this->gasPrice = $price;
-    return $this;
-  }
-  
-  public function setGasLimit($limit = null){
-    $this->gasLimit = $limit;
-    return $this;
-  }
-  
-  public function setValue($value = null){
-    $this->value = $value;
-    return $this;
-  }
-  
-  public function setCredential($credential){
-    $this->credential = $credential;
-    return $this;
-  }
-  
-  protected function netVersion(){
-    $cb = new Callback;
-    $this->web3->net->version($cb);
-    return $cb->result;
-  }
-  
-	protected function getTransactionCount($address){
-		$cb = new Callback;
-    $this->web3->eth->getTransactionCount($address,'pending',$cb);
-    return '0x' . $cb->result->toHex();
-	}  
-  
-  protected function estimateGasPrice(){
-    $cb = new Callback;
-    $this->web3->eth->gasPrice($cb);
-    return '0x' . $cb->result->toHex();
-  }
-  
-  protected function estimateGasUsage($tx){
-    //var_dump($tx);
-    $cb = new Callback;
-    $this->web3->eth->estimateGas($tx, $cb);
-    return '0x' . $cb->result->toHex();
-  }
-  
-  public function transact($tx): string{
-    if(!isset($this->credential)){
-      throw new Exception('credential not set');
+    protected $web3;
+
+    //context
+    protected $gasPrice;
+    protected $gasLimit;
+    protected $value;
+    protected $credential;
+
+    public function __construct($web3, $credential){
+        $this->web3 = $web3;
+        $this->credential = $credential;
     }
-    
-    $from = $this->credential->getAddress();
-    
-    $tx['from'] = $from;
-    
-    if(!isset($tx['nonce'])) {
-      $tx['nonce'] = $this->getTransactionCount($from);    
+
+    public function setGasPrice($price = null){
+        $this->gasPrice = $price;
+        return $this;
     }
-    
-    if(!isset($tx['chainId'])){
-      $tx['chainId'] = $this->netVersion();
+
+    public function setGasLimit($limit = null){
+        $this->gasLimit = $limit;
+        return $this;
     }
-    
-    if(!isset($tx['value'])) {
-      if(isset($this->value)){
-        $tx['value'] = $this->value;
-      } else{
-        $tx['value'] = '0x0';
-      }
+
+    public function setValue($value = null){
+        $this->value = $value;
+        return $this;
     }
-    
-    if(!isset($tx['gasPrice'])){
-      if(isset($this->gasPrice)){
-        $tx['gasPrice'] = $this->gasPrice;
-      } else {
-        $tx['gasPrice'] = $this->estimateGasPrice();
-      }
+
+    public function setCredential($credential){
+        $this->credential = $credential;
+        return $this;
     }
-    
-    if(!isset($tx['gasLimit'])){
-      if(isset($this->gasLimit)){
-        $tx['gasLimit'] = $this->gasLimit;
-      } else{
-        $tx['gasLimit'] = $this->estimateGasUsage($tx);
-      }
+
+    protected function netVersion(){
+        $cb = new Callback;
+        $this->web3->net->version($cb);
+        return $cb->result;
     }
-    
-    $stx = $this->credential->signTransaction($tx);
-    
-    $cb = new Callback;
-    $this->web3->eth->sendRawTransaction($stx,$cb);
-        
-    return $cb->result;
-  }  
-    
+
+    protected function getTransactionCount($address){
+        $cb = new Callback;
+        $this->web3->eth->getTransactionCount($address,'pending',$cb);
+        return '0x' . $cb->result->toHex();
+    }
+
+    protected function estimateGasPrice(){
+        $cb = new Callback;
+        $this->web3->eth->gasPrice($cb);
+        return '0x' . $cb->result->toHex();
+    }
+
+    protected function estimateGasUsage($tx){
+        //var_dump($tx);
+        $cb = new Callback;
+        $this->web3->eth->estimateGas($tx, $cb);
+        return '0x' . $cb->result->toHex();
+    }
+
+    public function transact($tx): string{
+        if(!isset($this->credential)){
+            throw new Exception('credential not set');
+        }
+
+        $from = $this->credential->getAddress();
+
+        $tx['from'] = $from;
+
+        if(!isset($tx['nonce'])) {
+            $tx['nonce'] = $this->getTransactionCount($from);
+        }
+
+        // if(!isset($tx['chainId'])){
+        //   $tx['chainId'] =  $this->netVersion();
+        // }
+
+        if(!isset($tx['value'])) {
+            if(isset($this->value)){
+                $tx['value'] = $this->value;
+            } else{
+                $tx['value'] = '0x0';
+            }
+        }
+
+        if(!isset($tx['gasPrice'])){
+            if(isset($this->gasPrice)){
+                $tx['gasPrice'] = $this->gasPrice;
+            } else {
+                $tx['gasPrice'] = $this->estimateGasPrice();
+            }
+        }
+
+        if(!isset($tx['gasLimit'])){
+            if(isset($this->gasLimit)){
+                $tx['gasLimit'] = $this->gasLimit;
+            } else{
+                $tx['chainId'] =  Utils::toHex($this->netVersion(),1);
+                $tx['gasLimit'] = $this->estimateGasUsage($tx);
+            }
+        }
+        $tx['chainId'] =  $this->netVersion();
+        $stx = $this->credential->signTransaction($tx);
+
+        $cb = new Callback;
+        $this->web3->eth->sendRawTransaction($stx,$cb);
+
+        return $cb->result;
+    }
+
 }
